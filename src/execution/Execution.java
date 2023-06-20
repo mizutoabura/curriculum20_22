b62c4882d174d266154b38123e3417175fe9e44c
rio <7noah13@gmail.com>
2023-06-20 22:03:26 +0900

    提出！0620

--- src/execution/Execution.java
+++ src/execution/Execution.java
@@ -6,9 +6,11 @@ public class Execution {
 
 	public static void main(String[] args) {
 
-		
+		// 情報を格納しているインスタンスを作成
 		Process process = new Process("こんにちは","日本","寿司","和食");
+		// 現在時刻を表示させるメソッドの呼び出し
 		process.nowTime();
+		
 	}
 
 }
--- src/process/Process.java
+++ src/process/Process.java
@@ -5,24 +5,37 @@ import java.util.Calendar;
 
 public class Process {
 
-	
+	// 挨拶を格納する変数
 	public String greeting;
+	// 国名を格納する変数
 	public String country;
+	// 食べ物を格納する変数
 	public String food;
+	// 食べ物の様式を格納する変数
 	public String style;
 	
+	// 受け取った引数をコメントにあてはめてコンソールに出力するコンストラクタ
 	public Process(String greeting, String country, String food, String style) {
+		
+		// 受け取った引数を変数に代入
 		this.greeting = greeting;
+		// 受け取った引数を変数に代入
 		this.country = country;
+		// 受け取った引数を変数に代入
 		this.food = food;
+		// 受け取った引数を変数に代入
 		this.style = style;
 		
+		// コンストラクタに変数をあてはめたメッセージを出力
 		System.out.println(greeting + "!" + "ここは" + country + "です！");
+		// コンストラクタに変数をあてはめたメッセージを出力
 		System.out.println("この" + food + "はうまい");
+		// コンストラクタに変数をあてはめたメッセージを出力
 		System.out.println(food + "は" + style + "です");
+		
 	}
 	
-
+	// 現在時刻を取得し形式にあてはめ表示
 	public  void nowTime() {
 
 		// 現在の日時を取得
